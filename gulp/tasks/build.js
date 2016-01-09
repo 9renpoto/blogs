c7b52846b4ef1fe6dc9f2c8a889a8d714047985c
Keisuke Kan <9renpoto@gmail.com>
2016-01-09 23:24:28 +0900

    remove clean

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,6 @@
 'use strict'
 var gulp = require('gulp')
-gulp.task('build', ['clean', 'dest'], function () {
+gulp.task('build', ['dest'], function () {
   var minifyHTML = require('gulp-minify-html')
   return gulp.src('dist/**/*.html')
     .pipe(minifyHTML({ empty: true }))
--- gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-var gulp = require('gulp')
-gulp.task('clean', function (callback) {
-  var del = require('del')
-
-  return del([
-    'dist'
-  ], callback)
-})
