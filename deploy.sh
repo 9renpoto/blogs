6272fbd1d246250ea7043122454740811ee92a4e
Keisuke Kan <9renpoto@gmail.com>
2016-06-04 19:53:21 +0900

    Update modules

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-cd 9renpoto.github.io
-git checkout master
-git pull origin master
-
-cd ..
 npm run build || exit 1
 
-cd 9renpoto.github.io
+cd dist
 
 msg="[ci skip] rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 
+git init
+git remote add git@github.com:9renpoto/9renpoto.github.io.git
 git add -A
 git commit -am "$msg"
 git push origin master
