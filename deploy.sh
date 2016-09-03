1b43da40679c905242c7cd064dadb11495d45bd7
Keisuke Kan <9renpoto@gmail.com>
2016-09-03 16:56:52 +0900

    chore(deploy) udpate deploy message

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
 npm run clean || exit 1
-
 mkdir dist && cd dist
 git init
 git remote add origin git@github.com:9renpoto/9renpoto.github.io.git
 git pull origin master
 cd ..
-
 npm run build || exit 1
-
-msg="[ci skip] rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-
 cd dist
-
 git add -A
-git commit -am "$msg"
+git commit -am "Updated"
 git push origin master
