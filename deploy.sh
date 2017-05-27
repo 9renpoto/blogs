ddf3c964a42c4352f0527bd40ad961407070f58f
Keisuke Kan <9renpoto@gmail.com>
2017-05-27 23:05:10 +0900

    chore(deploy) clean old files

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ mkdir dist && cd dist
 git init
 git remote add origin git@github.com:9renpoto/9renpoto.github.io.git
 git pull origin master
+find . -not -path '*/\.*' | xargs rm
 cd ..
 npm run build:content || exit 1
 cd dist
