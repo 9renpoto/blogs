6090d60de47173da1f12b610c60f3269ec05c04e
Keisuke Kan <9renpoto@gmail.com>
2017-06-15 20:49:46 +0900

    fix(deploy) fix remote (#189)

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 npm run clean || exit 1
-mkdir dist && cd dist
+mkdir -p dist && cd dist
 git init
-git remote add origin git@github.com:9renpoto/9renpoto.github.io.git
-git pull origin master
+git remote add blog git@github.com:9renpoto/9renpoto.github.io.git
+git pull blog master
 cd ..
 npm run build || exit 1
 cd dist
 git add -A
 git commit -am "Updated"
-git push origin master
+git push blog master
