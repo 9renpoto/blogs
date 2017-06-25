4ea8fa6dda7e1ee893bb0c0deff265b94c188710
Keisuke Kan <9renpoto@gmail.com>
2017-06-25 14:34:23 +0900

    fix(deploy) fix clean task

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init
 git remote add blog git@github.com:9renpoto/9renpoto.github.io.git
 git pull blog master
 cd ..
-npm run build || exit 1
+find dist -not -iwholename '*.git/*' | grep -v dist/.git | xargs rm && npm run build || exit 1
 cd dist
 git add -A
 git commit -am "Updated"
