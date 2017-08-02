06570ae28daf9019869cf2963c4c726e51028a60
Keisuke Kan <9renpoto@gmail.com>
2017-08-02 21:52:28 +0900

    chore(package) remove hugo files

--- gulp/tasks/dest.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-const gulp = require('gulp')
-const path = require('path')
-
-gulp.task('dest', () => {
-  return gulp.src(path.join(__dirname, '../../public/**'))
-    .pipe(gulp.dest(path.join(__dirname, '../../dist')))
-})
